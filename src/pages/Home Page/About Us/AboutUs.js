14b81b2008763bad98d707eea01d2be97408f286
Skander Ben Said <bensaid303@gmail.com>
2023-03-26 13:40:01 +0100

    changes

--- src/pages/Home Page/About Us/AboutUs.js
+++ src/pages/Home Page/About Us/AboutUs.js
@@ -44,3 +44,64 @@ const AboutUs = () => {
 };
 
 export default AboutUs;
+/*
+import "./App.css";
+import AddFiles from "./pages/AddFiles Page/AddFiles";
+import ListFiles from "./pages/AddFiles Page/ListFiles";
+
+import AddNotes from "./pages/Add Notes Page/AddNotes.js/AddNotes";
+import Notes from "./pages/Add Notes Page/Notes/Notes";
+
+import SignUp from "./pages/SignUp Page/SignUp";
+import SignIn from "./pages/SignIn Page/SignIn";
+
+import Profile from "./pages/Settings Pages/Pages/Profile";
+import Password from "./pages/Settings Pages/Pages/PasswordPage";
+import Interests from "./pages/Settings Pages/Pages/InterestsPage";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home Page/Home/Home";
+import Dashbord from "./pages/Courses Page/Home/Home";
+import AllCourses from "./pages/Courses Page/AllCourses/AllCourses";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Navbar from "./components/Right Sidebar/Sidebar";
+import HomeChat from "./pages/ChatPage/Homechat";
+import CoursesList from "./pages/Courses Page/CoursesList/CoursesList";
+import AboutUs from "./pages/Home Page/About Us/AboutUs";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/AddFiles" element={<AddFiles />} />
+          <Route path="/ListFiles" element={<ListFiles />} />
+
+          <Route path="/AddNotes" element={<AddNotes />} />
+          <Route path="/Notes" element={<Notes />} />
+
+          <Route path="/Chat" element={<HomeChat />} />
+
+          <Route path="/Sidebar" element={<Sidebar />} />
+          <Route path="/Navbar" element={<Navbar />} />
+
+          <Route path="/AboutUs" element={<AboutUs />} />
+
+          <Route path="/Dashbord" element={<Dashbord />} />
+          <Route path="/AllCourses" element={<AllCourses />} />
+          <Route path="/CoursesList" element={<CoursesList />} />
+
+          <Route path="/Signup" element={<SignUp />} />
+          <Route path="/Signin" element={<SignIn />} />
+
+          <Route path="/Settings/Profile" element={<Profile />} />
+          <Route path="/Settings/Password" element={<Password />} />
+          <Route path="/Settings/Interests" element={<Interests />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App; */
